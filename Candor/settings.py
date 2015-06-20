426c1ad6cb8b35ae8a72811394f91acd32dfdd41
Daniel Wyatt <dbwyatt23@gmail.com>
2015-06-20 17:04:10 -0600

    Merge for dev.

--- Candor/settings.py
+++ Candor/settings.py
@@ -26,7 +26,7 @@ elif os.path.dirname(BASE_DIR) == '/var/www/dev':
 SECRET_KEY = 'w58yt2*jmvxdwh-n3o*nf66#4+rl6^5172o&)gx69t04(5*8ow'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 if ENVIRONMENT == 'dev':
     DEBUG = True
 else:
@@ -104,6 +104,17 @@ else:
             'PORT': '5432',
         }
     }
+
+
+# Email
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 443
+EMAIL_HOST_USER = 'candorcontracts@gmail.com'
+EMAIL_HOST_PASSWORD = 'candorthis'
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
