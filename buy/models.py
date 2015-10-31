25be02e47db042e96aedfbb9c72ace9c6df95073
Chris Mena <bbmena@gmail.com>
2015-10-31 11:49:46 -0600

    initial buy models

--- buy/models.py
+++ buy/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from homepage import models as hmod
+from datetime import datetime
+
 
 # Define models here
+
+
+class SearchEntry(models.Model):
+    url = models.TextField(blank=False, null=False)
+    user = models.ForeignKey(hmod.Users)
+    date = models.DateTimeField(blank=False, null=False, default=datetime.now())
+
+    def table_sizer(self, user):
+        max_size = 100
+        current_searches = SearchEntry.objects.filter(user=user).order_by('date')
+        size = len(current_searches)
+
+        if size > max_size:
+            # delete everything after number 100
+            history_slice = current_searches[0:99]
+            SearchEntry.objects.filter(user=user).order_by('date')[100].delete()
+
+        return history_slice
+
+
+
+
+
+
+
+
