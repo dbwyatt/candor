7220d6c179dc82df0f462d2cd7a29a3bbc88b1d7
Conrad Beach <conradbeach@gmail.com>
2015-12-07 06:30:53 -0700

    Make a separate model for videos. Add male_or_female to Apartment model.

--- sell/models.py
+++ sell/models.py
@@ -11,10 +11,9 @@ class Post(models.Model):
     deposit = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=6)
     bounty = models.DecimalField(blank=True, null=True, decimal_places=2, max_digits=6)
     contracts = models.IntegerField(blank=False, null=False)
-    availability = models.DateField()
+    availability = models.DateField(blank=False, null=False)
     leaving = models.TextField(blank=False, null=False)
     amenity = models.ManyToManyField('Amenity', blank=True, null=True)
-    video = models.TextField(blank=True, null=True)
 
 
 class Amenity(models.Model):
@@ -26,6 +25,11 @@ class Picture(models.Model):
     picture = models.TextField(blank=False, null=False)
 
 
+class Video(models.Model):
+    post = models.ForeignKey(Post)
+    video = models.TextField(blank=False, null=False)
+
+
 class Apartment(models.Model):
     complex = models.TextField(blank=False, null=False)
     address = models.TextField(blank=False, null=False)
@@ -33,6 +37,7 @@ class Apartment(models.Model):
     longtitude = models.DecimalField(blank=False, null=False, decimal_places=8, max_digits=12)
     housing_type = models.TextField(blank=False, null=False, default='Apartment')
     single_or_married = models.TextField(blank=False, null=False, default='Single')
+    male_or_female = models.TextField(blank=True, null=True)
     bed_number = models.IntegerField(blank=False, null=False)
     bed_type = models.TextField(blank=False, null=False, default='Private')
     bath_number = models.DecimalField(blank=False, null=False, decimal_places=1, max_digits=3)
