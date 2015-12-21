a970f97f9368ade09f4d85ce58adff1317e35387
Conrad Beach <conradbeach@gmail.com>
2015-12-21 06:38:25 -0700

    Reorganize sell/models file.

--- sell/models.py
+++ sell/models.py
@@ -3,31 +3,19 @@ from homepage import models as hmod
 
 
 class Post(models.Model):
-    owner = models.ForeignKey(hmod.Users)
+    amenity = models.ManyToManyField('Amenity', blank=True, null=True)
     apartment = models.ForeignKey('Apartment', default=0)
-    title = models.TextField(blank=False, null=False)
-    description = models.TextField(blank=False, null=False)
-    price = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=8)
-    deposit = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=6)
+    availability = models.DateField(blank=False, null=False)
     bounty = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=6)
     contracts = models.IntegerField(blank=False, null=False)
-    availability = models.DateField(blank=False, null=False)
+    deposit = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=6)
+    description = models.TextField(blank=False, null=False)
+    facebook_link = models.TextField(blank=True, null=True)
     leaving = models.TextField(blank=False, null=False)
-    amenity = models.ManyToManyField('Amenity', blank=True, null=True)
-
-
-class Amenity(models.Model):
-    amenity = models.TextField(blank=False, null=False)
-
-
-class Picture(models.Model):
-    post = models.ForeignKey(Post)
-    picture = models.TextField(blank=False, null=False)
-
-
-class Video(models.Model):
-    post = models.ForeignKey(Post)
-    video = models.TextField(blank=False, null=False)
+    owner = models.ForeignKey(hmod.Users)
+    price = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=8)
+    status = models.TextField(blank=False, null=False)  # Options: active, inactive, sold
+    title = models.TextField(blank=False, null=False)
 
 
 class Apartment(models.Model):
@@ -42,3 +30,17 @@ class Apartment(models.Model):
     bed_type = models.TextField(blank=False, null=False, default='Private')
     bath_number = models.DecimalField(blank=False, null=False, decimal_places=1, max_digits=3)
     utilities = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=6)
+
+
+class Amenity(models.Model):
+    amenity = models.TextField(blank=False, null=False)
+
+
+class Picture(models.Model):
+    post = models.ForeignKey(Post)
+    picture = models.TextField(blank=False, null=False)
+
+
+class Video(models.Model):
+    post = models.ForeignKey(Post)
+    video = models.TextField(blank=False, null=False)
