52416590235f3167275a59794e5e3d0d36be32b7
Daniel Wyatt <dbwyatt23@gmail.com>
2016-03-17 00:31:50 -0600

    Finish new layout

--- homepage/scripts/index.js
+++ homepage/scripts/index.js
@@ -6,67 +6,138 @@ $(function () {
 	// 	$("#main-container").css({height: $(window).height()});
 	// }); // resize
 
-	var image = ['/static/homepage/media/apartment.jpg','/static/homepage/media/apartment2.jpg','/static/homepage/media/apartment3.jpg'];
-	var image_count = 1;
-	setInterval(function() {
-		var container = $('.home-background');
-		container.animate({opacity: 0}, 500, function() {
-			container.css('background-image', 'url(' + image[image_count] + ')');
-			container.animate({opacity: 1}, 500);
-		});
-		if (image_count == 2) {
-			image_count = 0;
-		}
-		else {
-			image_count++;
-		}
-	}, 10000);
+	// var image = ['/static/homepage/media/apartment.jpg','/static/homepage/media/apartment2.jpg','/static/homepage/media/apartment3.jpg'];
+	// var image_count = 1;
+	// setInterval(function() {
+	// 	var container = $('.home-background');
+	// 	container.animate({opacity: 0}, 500, function() {
+	// 		container.css('background-image', 'url(' + image[image_count] + ')');
+	// 		container.animate({opacity: 1}, 500);
+	// 	});
+	// 	if (image_count == 2) {
+	// 		image_count = 0;
+	// 	}
+	// 	else {
+	// 		image_count++;
+	// 	}
+	// }, 10000);
 
+	$('video').on('ended', function () {
+		this.load();
+		this.play();
+	});
 
-	$('#sign-up').ajaxForm(function(data) {
-		if (data == 'True') {
-			$('#sign-up').append("<div class='alert alert-info alert-dismissible success' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>Thank you! Your info has been saved. We're very excited to share this product with you soon! We will contact you when Candor is available.<br><br>If you have any questions regarding Candor or this registration, email us at <a href='mailto:candorcontracts@gmail.com'>candorcontracts@gmail.com</a>.</div>");
-			// setTimeout(function() {
-			// 	$('.close').trigger('click');
-			// }, 10000);
-			// $('#sign-up').find('input[type="submit"]').prop('disabled', true);
-			$('#sign-up input').not('input[type="submit"]').each(function() {
-				$(this).val('');
-			});
-			setTimeout(function() {
-				$.ajax({
-					type: 'GET',
-					url: '/homepage/index.register/',
-					success: function(data) {
-						$("#sign-up").remove();
-			            $('.overlay').append(data);
-					}
-				});
-			}, 10000);
-			$('#sign-up').on('click', '.close', function() {
-				$.ajax({
-					type: 'GET',
-					url: '/homepage/index.register/',
-					success: function(data) {
-						$("#sign-up").remove();
-			            $('.overlay').append(data);
-					}
-				});
-			});
+	var slide = slideCalc();
+
+	function slideCalc() {
+		var slide = 0;
+		if ($('h2.slide:in-viewport').length && $('h2.slide:in-viewport').width() / 4 > $('h2.slide:in-viewport').offset().left)
+			slide = $('h2.slide:in-viewport').offset().left;
+		else
+			slide = $('h2.slide:in-viewport').width() / 4;
+
+		return slide;
+	}
+
+	$('h2.slide:in-viewport').stop().delay(1000).animate(
+		{
+			opacity: 1,
+			marginLeft: "-" + slideCalc()
+		},
+		{
+			duration: 1000,
+			start: function() {
+				$(this).addClass('slid');
+			}
 		}
-		else {
-            $("#sign-up").remove();
-            $('.overlay').append(data);
-            $('.errorlist').each(function() {
-            	$(this).next().addClass('required').focus(function() {
-					if ($(this).hasClass('required')) {
-						$(this).val('');
+	);
+	$(window).scroll(function() {
+		$('h2.slide:above-the-top').removeClass('slid').removeAttr('style');
+		$('h2.slide:below-the-fold').removeClass('slid').removeAttr('style');
+		if (!$('h2.slide:in-viewport').hasClass('slid')) {
+			$('h2.slide:in-viewport').stop().delay(1000).animate(
+				{
+					opacity: 1,
+					marginLeft: "-" + slideCalc()
+				},
+				{
+					duration: 1000,
+					start: function() {
+						$(this).addClass('slid');
 					}
-					$(this).removeClass('required');
-				});
-            });
+				}
+			);
 		}
-	}); //register submit
+	});
+	$(window).resize(function() {
+		if ($('h2.slide:in-viewport').length && $('h2.slide:in-viewport').offset().left < 0) {
+			$('h2.slide:in-viewport').stop().animate({
+				marginLeft: "+=" + Math.abs($('h2.slide:in-viewport').offset().left)
+			}, 0);
+		}
+		else if ($('h2.slide:in-viewport').length && $('h2.slide:in-viewport').offset().left > 0) {
+			if ($('h2.slide:in-viewport').css('margin-left') < $('h2.slide:in-viewport').width() / 4) {	
+				$('h2.slide:in-viewport').stop().animate({
+					marginLeft: "-=" + Math.abs($('h2.slide:in-viewport').offset().left)
+				}, 0);
+			}
+			else {
+				$('h2.slide:in-viewport').stop().animate({
+					marginLeft: "-" + $('h2.slide:in-viewport').width() / 4
+				}, 0);
+			}
+		}
+	});
+
+	// $('#sign-up').ajaxForm(function(data) {
+	// 	if (data == 'True') {
+	// 		$('#sign-up').append("<div class='alert alert-info alert-dismissible success' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>Thank you! Your info has been saved. We're very excited to share this product with you soon! We will contact you when Candor is available.<br><br>If you have any questions regarding Candor or this registration, email us at <a href='mailto:candorcontracts@gmail.com'>candorcontracts@gmail.com</a>.</div>");
+	// 		// setTimeout(function() {
+	// 		// 	$('.close').trigger('click');
+	// 		// }, 10000);
+	// 		// $('#sign-up').find('input[type="submit"]').prop('disabled', true);
+	// 		$('#sign-up input').not('input[type="submit"]').each(function() {
+	// 			$(this).val('');
+	// 		});
+	// 		setTimeout(function() {
+	// 			$.ajax({
+	// 				type: 'GET',
+	// 				url: '/homepage/index.register/',
+	// 				success: function(data) {
+	// 					$("#sign-up").remove();
+	// 		            $('.overlay').append(data);
+	// 				}
+	// 			});
+	// 		}, 10000);
+	// 		$('#sign-up').on('click', '.close', function() {
+	// 			$.ajax({
+	// 				type: 'GET',
+	// 				url: '/homepage/index.register/',
+	// 				success: function(data) {
+	// 					$("#sign-up").remove();
+	// 		            $('.overlay').append(data);
+	// 				}
+	// 			});
+	// 		});
+	// 	}
+	// 	else {
+ //            $("#sign-up").remove();
+ //            $('.overlay').append(data);
+ //            $('.errorlist').each(function() {
+ //            	$(this).next().addClass('required').focus(function() {
+	// 				if ($(this).hasClass('required')) {
+	// 					$(this).val('');
+	// 				}
+	// 				$(this).removeClass('required');
+	// 			});
+ //            });
+	// 	}
+	// }); //register submit
+
+	$('#about a').on('click touchstart', function(e) {
+		e.preventDefault();
+		$('#meet-us').fadeIn();
+	});
 
 	$('#contact-form .btn').on('click', function() {
 		$(this).text('Sending...');
